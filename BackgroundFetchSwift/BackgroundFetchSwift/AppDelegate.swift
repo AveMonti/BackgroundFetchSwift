063fc76bf6d61f9d6684bff7dfe60569a6d1250d
Mateusz Chojnacki <chojnackimatt95@gmail.com>
2018-08-25 23:32:28 +0200

    work fine

--- BackgroundFetchSwift/BackgroundFetchSwift/AppDelegate.swift
+++ BackgroundFetchSwift/BackgroundFetchSwift/AppDelegate.swift
@@ -15,10 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        application.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
+        
         return true
     }
 
+    
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        let vc = (window?.rootViewController as? ViewController) ?? ViewController()
+        vc.backgroundAnnotation()
+        completionHandler(.newData)
+    }
+    
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- BackgroundFetchSwift/BackgroundFetchSwift/ViewController.swift
+++ BackgroundFetchSwift/BackgroundFetchSwift/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
+import UserNotifications
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
+    var locationManager = CLLocationManager()
+    var currentLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(0.00, 0.00)
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //LOCATION SETUP
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.locationManager.allowsBackgroundLocationUpdates = true
+        
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if error != nil {
+                print("Authorization Unsuccessfull")
+            }else {
+                print("Authorization Successfull")
+            }
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.last{
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+
+            self.currentLocation = center
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    func backgroundAnnotation(){
+        let contentNottyfication = UNMutableNotificationContent()
+        contentNottyfication.title = "5sec"
+        contentNottyfication.subtitle = "xD"
+        contentNottyfication.body = "Ilość \(self.currentLocation.latitude), \(self.currentLocation.longitude)"
+        contentNottyfication.badge = 1
 
+        let triger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        let request = UNNotificationRequest(identifier: "timer done", content: contentNottyfication, trigger: triger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 
 }
 
